58c91973054b9d11380a3e97bc41686b5f3dda62
Jarvist Moore Frost <jarvist@gmail.com>
2025-02-17 17:10:40 +0000

    Blown off enough dust to get it testing. Rattling the skeletons.

--- src/GBGA.jl
+++ src/GBGA.jl
@@ -1,18 +1,22 @@
 module GBGA
 
+using MolecularGraph
+using Evolutionary
+using Graphs
 import OpenSMILES 
-import Evolutionary
+import Evolutionary: population_size  # Import population_size
 
 greet() = print("Hello World!")
 
 # Override Evolutionary initial population
 import Evolutionary.initial_population
-function initial_population(method::M, individual::SimpleWeightedGraph) where {M<:Evolutionary.AbstractOptimizer}
-    OpenSMILES.ParseSMILES("Clc4cc2c(C(/c1ncccc1CC2)=C3/CCNCC3)cc4")
+function initial_population(method::M, individual::MolGraph) where {M<:Evolutionary.AbstractOptimizer}
+    # Return an array of molecules (population) of the specified size
+    return [MolecularGraph.smilestomol("Clc4cc2c(C(/c1ncccc1CC2)=C3/CCNCC3)cc4") 
+            for _ in 1:population_size(method)]
 end
 
 import Evolutionary.NonDifferentiable
-NonDifferentiable(f, x::GraphMol) = NonDifferentiable{Real,typeof(x)}(f, f(x), deepcopy(x), [0,])
-
+NonDifferentiable(f, x::MolGraph) = NonDifferentiable{Real,typeof(x)}(f, f(x), deepcopy(x), [0,])
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,11 +2,12 @@ using Test
 using GBGA
 using Evolutionary
 using MolecularGraph
+using Graphs
 
 # lots of these are by reference to the tests in the Evolutionary API tests
 # https://github.com/wildart/Evolutionary.jl/blob/eff5e0a23be5186d1d46bed298c263e8cedd8ff1/test/interface.jl
 
-mol=MolecularGraph.smilestomol("C")
+mol = MolecularGraph.smilestomol("C")
 
 struct TestOptimizer <: Evolutionary.AbstractOptimizer end
 method = TestOptimizer()
@@ -16,7 +17,7 @@ function population_size(method::TestOptimizer)
     return 1
 end
 
-pop=Evolutionary.initial_population(method, mol)
+pop = Evolutionary.initial_population(method, mol)
 @test length(pop) == Evolutionary.population_size(method)
 
 @test Evolutionary.NonDifferentiable(method, mol)
